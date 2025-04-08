af737144576abdd10eada04714853f10b196f7e0
luiza <d.luiza@aluno.ifsp.edu.br>
2025-04-08 15:51:18 -0300

    01

---
+++ java.js
@@ -0,0 +1,10 @@
+// 01
+function exibirVariaveis() {
+    let nome = "Maria";
+    let idade = 25;
+    let estudante = true;
+
+    console.log("Nome:", nome);
+    console.log("Idade:", idade);
+    console.log("Estudante:", estudante);
+}
